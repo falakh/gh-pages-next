f05c7bc4c120e6ddb6bbcf6d2eed328f343c7fca
falakh <zulfikarfahmi38@gmail.com>
2022-06-02 09:10:25 +0000

    [skip ci] Updates

---
+++ _next/static/j8Th9-Tl89VvlmJpwoo9W/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/chunks/pages/index-8fa44bc661ff6ebc.js"],"/_error":["static/chunks/pages/_error-98e801c646abf33c.js"],"/about":["static/chunks/pages/about-8bf50721b654245a.js"],sortedPages:["/","/_app","/_error","/about"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
